d03f0b3c62e297b6e6a5cd2b14a937a63a3fae2e
Jia Jia <angersax@sina.com>
2022-07-27 14:59:35 +0800

    feat(arg): add arguments for ninja

--- src/arg/arg.rs
+++ src/arg/arg.rs
@@ -19,16 +19,94 @@ impl Argument {
             concat!(env!("CARGO_PKG_VERSION"), "-build-", env!("build")).to_string();
 
         let matches = Command::new("ninja")
-            .version(&*self.version_info)
             .arg(
-                Arg::new("config_file")
-                    .short('c')
-                    .long("config-file")
-                    .value_name("NAME")
-                    .help("Config file (.yml)")
+                Arg::new("working_dir")
+                    .short('C')
+                    .value_name("DIR")
+                    .help("change to DIR before doing anything else")
                     .takes_value(true)
                     .required(false),
             )
+            .arg(
+                Arg::new("enable_debug")
+                    .short('d')
+                    .value_name("MODE")
+                    .help("enable debugging (use '-d list' to list modes)")
+                    .takes_value(true)
+                    .required(false),
+            )
+            .arg(
+                Arg::new("input_file")
+                    .short('f')
+                    .value_name("FILE")
+                    .help("specify input build file [default=build.ninja]")
+                    .takes_value(true)
+                    .required(false),
+            )
+            .arg(
+                Arg::new("parallelism")
+                    .short('j')
+                    .value_name("N")
+                    .help("run N jobs in parallel (0 means infinity) [default=18 on this system]")
+                    .takes_value(true)
+                    .required(false),
+            )
+            .arg(
+                Arg::new("failures_allowed")
+                    .short('k')
+                    .value_name("N")
+                    .help("keep going until N jobs fail (0 means infinity) [default=1]")
+                    .takes_value(true)
+                    .required(false),
+            )
+            .arg(
+                Arg::new("max_load_average")
+                    .short('l')
+                    .value_name("N")
+                    .help("do not start new jobs if the load average is greater than N")
+                    .takes_value(true)
+                    .required(false),
+            )
+            .arg(
+                Arg::new("dry_run")
+                    .short('n')
+                    .help("dry run (don't run commands but act like they succeeded)")
+                    .takes_value(false)
+                    .required(false),
+            )
+            .arg(
+                Arg::new("quiet")
+                    .short('q')
+                    .long("quiet")
+                    .help("don't show progress status, just command output")
+                    .takes_value(false)
+                    .required(false),
+            )
+            .arg(
+                Arg::new("tool")
+                    .short('t')
+                    .value_name("TOOL")
+                    .help("run a subtool (use '-t list' to list subtools)")
+                    .takes_value(true)
+                    .required(false),
+            )
+            .arg(
+                Arg::new("verbose")
+                    .short('v')
+                    .long("verbose")
+                    .help("show all command lines while building")
+                    .takes_value(false)
+                    .required(false),
+            )
+            .arg(
+                Arg::new("enable_warn")
+                    .short('w')
+                    .value_name("FLAG")
+                    .help("adjust warnings (use '-w list' to list warnings)")
+                    .takes_value(true)
+                    .required(false),
+            )
+            .version(&*self.version_info)
             .get_matches();
 
         match matches.value_of("config_file") {
