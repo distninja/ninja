1c4177d88d6f09a2d8c0116a8fb90d03fd1089c7
Jia Jia <admin@example.com>
2025-05-14 05:22:56 +0800

    feat(release): add github action for release

--- script/build.sh
+++ script/build.sh
@@ -4,4 +4,3 @@
 # Enable function trace: FUNCTION_TRACE (--function-trace)
 cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DANDROID_SOONG=ON -B release-build
 cmake --build release-build --parallel --config Release
-strip release-build/ninja
