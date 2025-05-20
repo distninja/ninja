5857b3b822dc3f388f7bfbeb3b601847d71c472e
Jia <49358172+craftslab@users.noreply.github.com>
2025-05-20 04:41:57 +0000

    feat(script): add support for building on ubuntu and windows

--- script/build.sh
+++ script/build.sh
@@ -2,5 +2,18 @@
 
 # Enable android soong: ANDROID_SOONG (--android-soong)
 # Enable function trace: FUNCTION_TRACE (--function-trace)
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DANDROID_SOONG=ON -B release-build
+
+if grep -qi microsoft /proc/version; then
+    echo "Running on Windows Subsystem for Linux"
+    cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DANDROID_SOONG=ON -B release-build
+elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
+    echo "Running on Windows"
+    cmake -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Release -DANDROID_SOONG=ON -B release-build
+elif [[ -f /etc/os-release ]] && grep -qi ubuntu /etc/os-release; then
+    echo "Running on Ubuntu"
+    cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DANDROID_SOONG=ON -B release-build
+else
+    echo "Unknown OS"
+fi
+
 cmake --build release-build --parallel --config Release
